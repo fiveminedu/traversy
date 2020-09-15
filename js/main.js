248d19e5d52d4bf8a97b495108225f2b426a124b
fiveminedu <60638106+fiveminedu@users.noreply.github.com>
2020-09-15 20:54:32 +0530

    JS sliding update

--- js/main.js
+++ js/main.js
@@ -1,7 +1,4 @@
-const signUpButton = document.getElementById("signUp");
-const signInButton = document.getElementById("signIn");
-const container = document.getElementById("container");
-
-signUpButton.addEventListener('click', () => container.classList.add('right-panel-active'));
-
-signInButton.addEventListener('click', () => container.classList.remove('right-panel-active'));
+window.addEventListener('load', function(){
+    document.getElementById("signUp").addEventListener('click', () => document.getElementById("container").classList.add('right-panel-active'));
+    document.getElementById("signIn").addEventListener('click', () => document.getElementById("container").classList.remove('right-panel-active'));
+})
